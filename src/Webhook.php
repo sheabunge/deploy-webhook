01471b19539468233928a40e492b5e6db4414ddb
Shea Bunge <shea@sheabunge.com>
2022-08-07 01:23:05 +1000

    Simplify conditional

--- src/Webhook.php
+++ src/Webhook.php
@@ -13,10 +13,7 @@ class Webhook {
 	}
 
 	public function validate_secret( $secret_token ) {
-		if ( ! isset( $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ) ) {
-			return false;
-		}
-
-		return 'sha256=' . hash( 'sha256', $secret_token ) === $_SERVER['HTTP_X_HUB_SIGNATURE_256'];
+		return isset( $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ) &&
+		       $_SERVER['HTTP_X_HUB_SIGNATURE_256'] === 'sha256=' . hash( 'sha256', $secret_token );
 	}
 }
