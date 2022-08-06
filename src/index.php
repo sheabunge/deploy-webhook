2b6dda59f474e25d234f1291cd6d4cbe82481fc1
Shea Bunge <shea@sheabunge.com>
2022-08-07 02:31:59 +1000

    Include event name in error message

--- src/index.php
+++ src/index.php
@@ -10,7 +10,7 @@ $webhook = new Webhook( SECRET_TOKEN );
 $webhook->verify_signature();
 
 if ( ! $webhook->is_push_event() ) {
-	trigger_error( 'Only responds to push events', E_USER_ERROR );
+	trigger_error( sprintf( 'Expected push event, received %s', $webhook->get_event() ), E_USER_ERROR );
 }
 
 $runner = new Runner( WORKING_DIR );
